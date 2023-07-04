fc2f91236de6d721ab4ddc6a2504b4adc76b6539
Julien Oculi <xdrz.jot.sr@gmail.com>
2023-07-05 00:59:30 +0200

    refactor: rewrite all API to class based approach
    Improve dev experience by using webui instance instead of dealing with pointer to window

--- src/types.ts
+++ src/types.ts
@@ -1,3 +1,4 @@
+import { WebUi } from "../mod.ts";
 import { loadLib } from "./lib.ts";
 
 export type Usize = number | bigint;
@@ -5,11 +6,11 @@ export type Usize = number | bigint;
 export type BindCallback<
   T extends string | number | boolean | undefined | void,
 > = (
-  event: Event,
-) => T;
+  event: WebUiEvent,
+) => T | Promise<T>;
 
-export interface Event {
-  win: Usize;
+export interface WebUiEvent {
+  window: WebUi;
   eventType: number;
   element: string;
   data: string;
