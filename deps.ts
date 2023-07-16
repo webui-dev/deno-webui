7e693da3f5e5452fab2de3b0d095cbf33037748e
Julien Oculi <xdrz.jot.sr@gmail.com>
2023-07-16 21:33:20 +0200

    fix: :arrow_up: update lib url to serve current API

--- deps.ts
+++ deps.ts
@@ -2,13 +2,13 @@
 export {
   //@ts-ignore ejm serve { b64: string }
   default as webui2Windows,
-} from "https://ejm.sh/deno.land/x/webui@2.3.0/src/webui-2-x64.dll" assert {
+} from "https://ejm.sh/raw.githubusercontent.com/webui-dev/deno-webui/2930d88868a629ecc07d48320489fa0dc1b7fe1d/src/webui-2-x64.dll" assert {
   type: "json",
 };
 export {
   //@ts-ignore ejm serve { b64: string }
   default as webui2Linux,
-} from "https://ejm.sh/deno.land/x/webui@2.3.0/src/webui-2-x64.so" assert {
+} from "https://ejm.sh/raw.githubusercontent.com/webui-dev/deno-webui/2930d88868a629ecc07d48320489fa0dc1b7fe1d/src/webui-2-x64.so" assert {
   type: "json",
 };
 export {
