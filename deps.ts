d535f68195c6d9ec9528b499369581a0e8380e5f
Hassan DRAGA <dragahassan@gmail.com>
2025-04-07 19:22:17 -0400

    Update deps.ts

--- deps.ts
+++ deps.ts
@@ -19,7 +19,7 @@ function getBaseLibName(): string {
       }
       break;
     case "darwin": // macOS
-      baseName = useNightly ? "libwebui-2.dylib" : "webui-2.dylib";
+      baseName = "libwebui-2.dylib";
       // Validate architecture for macOS
       if (Deno.build.arch !== "x86_64" && Deno.build.arch !== "aarch64") {
         throw new Error(
@@ -28,7 +28,7 @@ function getBaseLibName(): string {
       }
       break;
     default: // Linux and other Unix-like OSes
-      baseName = useNightly ? "libwebui-2.so" : "webui-2.so";
+      baseName = "libwebui-2.so";
       // Validate architecture for Linux/others
       if (Deno.build.arch !== "x86_64" && Deno.build.arch !== "aarch64") {
         throw new Error(
