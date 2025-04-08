79802d5eecb7acfb8e610be65b42c559b84cf16f
Hassan DRAGA <dragahassan@gmail.com>
2025-04-07 20:00:53 -0400

    up version from 2.5.3 to 2.5.4

--- src/webui.ts
+++ src/webui.ts
@@ -1,5 +1,5 @@
 /*
-  WebUI Deno 2.5.3
+  WebUI Deno 2.5.4
   http://webui.me
   https://github.com/webui-dev/deno-webui
   Copyright (c) 2020-2025 Hassan Draga.
@@ -1123,7 +1123,7 @@ export class WebUI {
   }
 
   static get version(): string {
-    return "2.5.3";
+    return "2.5.4";
   }
 }
 
